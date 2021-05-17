6ac617d7af5656f37dd68bd53f25c45efbfa1625
theredrover <mykhno2001@gmail.com>
2021-05-17 10:43:59 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import random
+
+import telebot
+
+import os
+
+from datetime import datetime
+
+token = '1879111908:AAGpn6-hdnTfgugWp_DlL_Dn-z6An_62-YM'
+
+if __name__ == '__main__':
+
+
+    bot = telebot.TeleBot(token)
+
+
+    @bot.message_handler(commands=['start'])
+    def send_message(message):
+        print("hello mess", message.chat.id)
+        bot.reply_to(message, "Hello")
+
+    @bot.message_handler(commands=['create_queue'])
+    def send_message(message):
+        bot.reply_to(message, "send command with list of your group.txt")
+
+
+    @bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
+    def send_message(message):
+        print("geted reqИ никто н", message.chat.id)
+        try:
+            file_name = message.document.file_name
+            if(not file_name=="group.txt"):
+                bot.send_message(message.chat.id, "File must be called 'group.txt'")
+                return
+            file_id_info = bot.get_file(message.document.file_id)
+            print("geted", message.chat.id)
+            downloaded_file = bot.download_file(file_id_info.file_path)
+            src = file_name
+            with open(src, 'wb') as loaded_file:
+                loaded_file.write(downloaded_file)
+            with open(src, 'r') as file:
+                lines = file.read()
+                lines = list(lines.split('\n'))
+                random.shuffle(lines)
+                random.shuffle(lines)
+                random.shuffle(lines)
+                answer = ""
+                i=1
+                for line in lines:
+                    answer += str(i)+ " - "+line+'\n'
+                    i+=1
+                bot.send_message(message.chat.id, answer)
+            os.remove(src)
+        except Exception as ex:
+            print(ex)
+
+
+    bot.polling()
+
+
