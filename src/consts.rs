42d5b69eb552523640284427863ef60166eac487
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2021-05-19 22:54:46 +0300

    commands

---
+++ src/consts.rs
@@ -0,0 +1,2 @@
+pub const HANDLER_PORT: &str = "BOT_CUSTOMHANDLER_PORT";
+pub const BOT_NAME: &str = "BOT_NAME";
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use std::env;
+mod consts;
 
-use chrono::{TimeZone, Timelike, Utc};
-use chrono_tz::Europe::Athens;
-use teloxide::{prelude::*, types::InputFile};
-use tokio_stream::wrappers::UnboundedReceiverStream;
-
-use chrono::DateTime;
-use chrono_tz::Tz;
+use core::panic;
 use futures::lock::Mutex;
-use std::collections::HashMap;
+use std::env;
 use std::net::Ipv4Addr;
 use std::sync::Arc;
+use std::{collections::HashMap, error::Error};
+use teloxide::{
+    prelude::*,
+    types::{File, InputFile},
+    utils::command::BotCommand,
+};
+use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::{http::Response, Filter};
 
 #[tokio::main]
 async fn main() {
     run().await;
+}
+
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum QueueCommand {
+    #[command(description = "Obtain help.")]
+    Help,
+    #[command(description = "Swap positions in the queue.", parse_with = "split")]
+    Swap(u32, u32),
+    #[command(description = "Create a new queue from a file.")]
+    CreateQueue,
 }
 
 async fn run() {
@@ -28,9 +40,7 @@ async fn run() {
             Some(name) => Response::builder().body(format!("Hello, {}. This HTTP triggered function executed successfully.", name)),
             None => Response::builder().body(String::from("This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.")),
         });
-
-    let port_key = "BOT_CUSTOMHANDLER_PORT";
-    let port: u16 = match env::var(port_key) {
+    let port: u16 = match env::var(consts::HANDLER_PORT) {
         Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
         Err(_) => 3000,
     };
@@ -39,74 +49,27 @@ async fn run() {
 
     let bot = Bot::from_env();
 
-    let map = Arc::new(Mutex::new(HashMap::new()));
+    let bot_name = env::var(consts::BOT_NAME)
+        .expect(format!("You must provide the {} env variable", consts::BOT_NAME).as_str());
 
-    let dispatch = async move {
-        let x = Dispatcher::new(bot).messages_handler(|rx| handle_messages(rx, map));
-        x.dispatch().await
-    };
+    let repl = teloxide::commands_repl(bot, bot_name, answer);
 
-    tokio::join!(dispatch, serve);
+    tokio::join!(repl, serve);
 }
 
-async fn handle_messages(
-    rx: DispatcherHandlerRx<Bot, Message>,
-    chat_last_self_msg: Arc<Mutex<HashMap<i64, DateTime<Tz>>>>,
-) {
-    let ref_chat_map = &chat_last_self_msg;
-    UnboundedReceiverStream::new(rx)
-        .for_each_concurrent(None, |msg| async move {
-            log::info!("msg: {:?}", msg.update);
-            match &msg.update.kind {
-                teloxide::types::MessageKind::Common(_) => {
-                    let time = Utc::now().naive_utc();
-                    let time = Athens.from_utc_datetime(&time);
-                    let hour = time.hour();
-                    let late_at_night = hour <= 6;
-                    log::info!(
-                        "time: {}; hour: {}; late_at_night?: {}",
-                        time,
-                        hour,
-                        late_at_night
-                    );
+async fn answer(
+    cx: UpdateWithCx<Bot, Message>,
+    command: QueueCommand,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        QueueCommand::Help => cx.answer(QueueCommand::descriptions()).send().await?,
+        QueueCommand::Swap(_, _) => {
+            unimplemented!()
+        }
+        QueueCommand::CreateQueue => {
+            unimplemented!()
+        }
+    };
 
-                    let debug_respond_always = env::var("TG_BOT_RESPOND_ALWAYS_DEBUG");
-                    log::info!("debug?: {}", debug_respond_always.is_ok());
-
-                    let debug_ignore_late_at_night = env::var("TG_BOT_IGNORE_NIGHT_DEBUG");
-                    log::info!("ignore night?: {}", debug_ignore_late_at_night.is_ok());
-
-                    if debug_ignore_late_at_night.is_ok() || late_at_night {
-                        let chat_map = ref_chat_map.clone();
-                        let mut chat_map = chat_map.lock().await;
-
-                        if debug_respond_always.is_ok()
-                            || chat_map
-                                .get(&msg.update.chat_id())
-                                .map(|prev_time| {
-                                    let mins = (time - *prev_time).num_minutes();
-                                    log::info!(
-                                        "minutes passed from the time of the last bots message: {}",
-                                        mins
-                                    );
-                                    mins > 60
-                                })
-                                .unwrap_or(true)
-                        {
-                            log::info!("sending a message...");
-                            let mut resp =
-                                msg.answer_photo(InputFile::File("resources/img.jpg".into()));
-                            resp.reply_to_message_id = Some(msg.update.id);
-                            resp.send().await.log_on_error().await;
-
-                            log::info!("message sent.");
-
-                            chat_map.insert(msg.update.chat_id(), time);
-                        }
-                    }
-                }
-                _ => (),
-            }
-        })
-        .await;
+    Ok(())
 }
